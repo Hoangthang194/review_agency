bfb98fea189280a68285d7968d5118c6f75bb66a
Hoangthang194 <thangthanhthat10a3@gmail.com>
2026-01-14 12:20:38 +0700

    update

---
+++ ecosystem.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    apps: [
+      {
+        name: "review-agency",
+        script: "C:\\Program Files\\nodejs\\npm.cmd",
+        args: "start",
+        interpreter: "cmd",
+        cwd: "C:\\Users\\Administrator\\Documents\\review_agency",
+        autorestart: true,
+        env: {
+          NODE_ENV: "production"
+        }
+      }
+    ]
+  }
+  
